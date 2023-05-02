04eef7055688dec36f028c4e8a7e64e1d6a3d462
valentina-rojas <valentinarojas618@gmail.com>
2023-05-02 12:51:25 -0300

    cambio de nombre en variable player

--- assets/scenes/Game.js
+++ assets/scenes/Game.js
@@ -19,7 +19,7 @@ export default class  extends Phaser.Scene {
     preload() {
         this.load.image("sky", "./assets/images/Cielo.png");
         this.load.image("platform", "./assets/images/platform.png");
-        this.load.image("ninja", "./assets/images/Ninja.png");
+        this.load.image("player", "./assets/images/Ninja.png");
         this.load.image(TRIANGULO, "./assets/images/Triangulo.png");
         this.load.image(ROMBO, "./assets/images/Rombo.png");
         this.load.image(CUADRADO, "./assets/images/Cuadrado.png")
@@ -35,7 +35,7 @@ export default class  extends Phaser.Scene {
         this.add.image(400, 300, "sky").setScale(0.555)
         
 
-        this.ninja = this.physics.add.sprite(100, 500, "ninja")
+        this.player = this.physics.add.sprite(100, 500, "player")
 
 
         this.plataformasPropias = this.physics.add.staticGroup();
@@ -47,10 +47,10 @@ export default class  extends Phaser.Scene {
   
 
 
-        this.physics.add.collider (this.ninja, this.plataformasPropias);
+        this.physics.add.collider (this.player, this.plataformasPropias);
         this.physics.add.collider (this.shapeGroup, this.plataformasPropias);
         
-        this.physics.add.overlap(this.ninja, this.shapeGroup, this.collectShape, null,  this );
+        this.physics.add.overlap(this.player, this.shapeGroup, this.collectShape, null,  this );
 
 
         this.cursors = this.input.keyboard.createCursorKeys();
@@ -69,22 +69,22 @@ export default class  extends Phaser.Scene {
     update() {
 
       if (this.cursors.left.isDown){
-        this.ninja.setVelocityX(-PLAYER_MOVEMENTS.x);
+        this.player.setVelocityX(-PLAYER_MOVEMENTS.x);
       } else if  (this.cursors.right.isDown) {
-        this.ninja.setVelocityX(PLAYER_MOVEMENTS.x);
-      } else {this.ninja.setVelocityX(0);
+        this.player.setVelocityX(PLAYER_MOVEMENTS.x);
+      } else {this.player.setVelocityX(0);
       }
       
 
-      if (this.cursors.up.isDown && this.ninja.body.touching.down) {
-         this.ninja.setVelocityY(-PLAYER_MOVEMENTS.y);
+      if (this.cursors.up.isDown && this.player.body.touching.down) {
+         this.player.setVelocityY(-PLAYER_MOVEMENTS.y);
       }
     
     }
 
 
 
-    collectShape(ninja, shape) {
+    collectShape(player, shape) {
         console.log("figura recolectada");
         shape.disableBody(true, true);
 
