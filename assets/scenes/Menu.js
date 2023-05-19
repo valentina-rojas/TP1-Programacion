57d4fb7638c12582490e17119a09312357b998bb
valentina-rojas <valentinarojas618@gmail.com>
2023-05-19 14:51:44 -0300

    se agrego imagen en menu

--- assets/scenes/Menu.js
+++ assets/scenes/Menu.js
@@ -7,27 +7,32 @@ export default class extends Phaser.Scene {
 
   preload() {
     this.load.image("sky", "./assets/images/Cielo.png");
+    this.load.image("ninja", "./assets/images/Ninja.png");
   }
 
   create() {
-
     //imagen de fondo
     this.add.image(400, 300, "sky").setScale(0.555);
 
+    //imagen ninja
+    this.add.image(400, 520, "ninja");
+
     //título del juego
-    this.add.text(400,200, "NINJA MONCHO",{
-          fontFamily: "roboto",
-          fontSize: 60,
-          color: "#ffffff",
-          shadow: {
-            offsetX: 3,
-            offsetY: 3,
-            color: "#000000",
-            blur: 5,
-            stroke: false,
-            fill: true,
-          },
-        } ).setOrigin(0.5);
+    this.add
+      .text(400, 200, "NINJA MONCHO", {
+        fontFamily: "roboto",
+        fontSize: 60,
+        color: "#ffffff",
+        shadow: {
+          offsetX: 3,
+          offsetY: 3,
+          color: "#000000",
+          blur: 5,
+          stroke: false,
+          fill: true,
+        },
+      })
+      .setOrigin(0.5);
 
     //botón para empezar el juego
     const button = this.add
@@ -49,17 +54,15 @@ export default class extends Phaser.Scene {
       .setOrigin(0.5)
       .setInteractive();
 
+    // cambiar el color de fondo cuando el puntero se coloca sobre el botón
+    button.on("pointerover", () => {
+      button.setStyle({ backgroundColor: "#888888" });
+    });
 
-// cambiar el color de fondo cuando el puntero se coloca sobre el botón
-button.on("pointerover", () => {
-  button.setStyle({ backgroundColor: "#888888" });
-});
-
-// volver al color de fondo cuando el puntero sale del botón
-button.on("pointerout", () => {
-  button.setStyle({ backgroundColor: "#000000" });
-});
-
+    // volver al color de fondo cuando el puntero sale del botón
+    button.on("pointerout", () => {
+      button.setStyle({ backgroundColor: "#000000" });
+    });
 
     // al presionar el boton llevar a la escena de juego
     button.on("pointerup", () => {
